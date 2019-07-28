72098bc2f3e995f1fae7d8d7e6922143b6c5df57
samat <somych1@gmail.com>
2019-07-28 13:31:38 -0500

    models created

---
+++ src/main/java/com/trilogy/adminapiservice/model/LevelUp.java
@@ -0,0 +1,63 @@
+package com.trilogy.adminapiservice.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class LevelUp {
+    private int levelUpId;
+    private int customerId;
+    private int points;
+    @JsonSerialize(using = ToStringSerializer.class)
+    private LocalDate memberDate;
+
+    public int getLevelUpId() {
+        return levelUpId;
+    }
+
+    public void setLevelUpId(int levelUpId) {
+        this.levelUpId = levelUpId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public LocalDate getMemberDate() {
+        return memberDate;
+    }
+
+    public void setMemberDate(LocalDate memberDate) {
+        this.memberDate = memberDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LevelUp levelUp = (LevelUp) o;
+        return levelUpId == levelUp.levelUpId &&
+                customerId == levelUp.customerId &&
+                points == levelUp.points &&
+                Objects.equals(memberDate, levelUp.memberDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(levelUpId, customerId, points, memberDate);
+    }
+}
