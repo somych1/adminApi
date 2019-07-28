43919e7a11ecfe00f4c30502b398369fa132848e
samat <somych1@gmail.com>
2019-07-28 13:13:55 -0500

    first commit

---
+++ src/main/java/com/trilogy/adminapiservice/AdminApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogy.adminapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilogy/adminapiservice/AdminApiServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trilogy.adminapiservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminApiServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
